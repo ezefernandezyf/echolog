1df13aa228c4935435a0f54c7dd237d76885242e
Ezefernandezyf <ezefernandezyf@gmail.com>
2026-05-11 13:42:33 -0300

    fix(server): add try/catch to comments controller handlers

--- server/src/comments/comments.controller.ts
+++ server/src/comments/comments.controller.ts
@@ -1,12 +1,20 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import { commentsService } from './comments.service.js';
 
-export const listComments = async (req: Request, res: Response) => {
-  const data = await commentsService.list(req.params.postId as string);
-  res.status(200).json(data);
+export const listComments = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await commentsService.list(req.params.postId as string);
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const createComment = async (req: Request, res: Response) => {
-  const data = await commentsService.create(req.params.postId as string, req.body, req.userId!);
-  res.status(201).json(data);
+export const createComment = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await commentsService.create(req.params.postId as string, req.body, req.userId!);
+    res.status(201).json(data);
+  } catch (err) {
+    next(err);
+  }
 };
